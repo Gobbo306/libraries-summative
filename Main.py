c5f0228460ea38bfb0f49e5bcf5863d5282b7bca
Gobbo306 <54857960+Gobbo306@users.noreply.github.com>
2019-10-31 10:04:40 -0600

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,14 @@
+#------------------------------------------------
+#Dylan Friesen
+#Libraries Formative - Main Program
+#October 28, 2019
+#------------------------------------------------
+
+#------------------Imports---------------------
+import time
+from other import basic_print
+from other import more_stuff
+
+basic_print.choose_basic()
+print (basic_print.cool_list)
+print (basic_print.john)
